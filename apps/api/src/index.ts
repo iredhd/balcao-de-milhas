e89a4ed154cac4120af369702b28affad74f3cb8
Ighor Redhd <iredhd.dev@gmail.com>
2023-11-30 18:37:14 -0300

    add news handler

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,6 +4,7 @@ import cors from 'cors'
 import morgan from 'morgan'
 import { config } from 'dotenv'
 import { logMiddleware } from './middlewares'
+import { db } from './db'
 
 const app = express()
 
@@ -25,8 +26,18 @@ app.post('/webhook/bdm', (req, res) => {
   return res.status(200).json({})
 })
 
-app.post('/webhook/milha-news', (req, res) => {
-  return res.status(200).json({})
+app.post('/webhook/milha-news', async (req, res) => {
+  const title = req.body.title
+  const link = req.body.link
+
+  await db.news.create({
+    data: {
+      title,
+      link
+    }
+  })
+
+  return res.status(201).end()
 })
 
 app.use('/bid', bid)
