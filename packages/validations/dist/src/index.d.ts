67e1b35902f112fa5c9a5479dbff91f4e9598bfc
Ighor Redhd <iredhd.dev@gmail.com>
2023-12-06 16:27:18 -0300

    remove dist

--- packages/validations/dist/src/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const INTERNAL_ERROR = "Erro interno. Por favor, entre em contato com o suporte.";
--- packages/validations/dist/src/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.INTERNAL_ERROR = void 0;
-exports.INTERNAL_ERROR = 'Erro interno. Por favor, entre em contato com o suporte.';
