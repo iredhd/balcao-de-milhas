87731a4802b7e12346bb9c25da8efb2cf7980ad3
Ighor Redhd <iredhd.dev@gmail.com>
2024-01-08 21:48:01 -0300

    bid webhook

--- apps/api/src/routes/webhook.ts
+++ apps/api/src/routes/webhook.ts
@@ -51,7 +51,53 @@ webhook.post('/milha-news', async (req, res) => {
 })
 
 webhook.post('/bid', async (req, res) => {
-  return res.status(201).end()
+  const {
+    created_at,
+    offer_id,
+    amount,
+    price,
+    company,
+    pax,
+    cancell_return_percentage,
+    direction,
+    is_vcm,
+    claims,
+    is_mastermiles,
+    is_mentoria,
+    member_since,
+    recommendations,
+    date_of_first_join,
+    number_of_communities,
+  } = req.body
+
+  try {
+    await db.bid.create({
+      data: {
+        created_at: new Date(created_at),
+        amount: Number(amount),
+        cancell_return_percentage: Number(cancell_return_percentage),
+        company,
+        direction,
+        is_vcm: Boolean(parseInt(is_vcm)),
+        offer_id: Number(offer_id),
+        pax: Number(pax),
+        price: Number(price),
+        claims: Number(claims),
+        date_of_first_join: new Date(date_of_first_join),
+        is_mastermiles: Boolean(parseInt(is_mastermiles)),
+        is_mentoria: Boolean(parseInt(is_mentoria)),
+        member_since: new Date(member_since),
+        number_of_communities: Number(number_of_communities),
+        recommendations: Number(recommendations)
+      }
+    })
+
+    return res.status(201).end()
+  } catch (e) {
+    return res.status(500).json({
+      message: (e as Error)?.message
+    })
+  }
 })
 
 webhook.post('/bdm', async (req, res) => {
